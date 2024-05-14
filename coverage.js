0cac8db9f3b3381fbaca64dad1fbb12b5e4b117e
shravan20 <mrshravankumarb@gmail.com>
2024-05-14 10:39:06 +0530

    docs: serve coverage and include it in docs

---
+++ coverage.js
@@ -0,0 +1,44 @@
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const PORT = 8000;
+const COVERAGE_DIR = path.join(__dirname, 'coverage', 'lcov-report');
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(
+    COVERAGE_DIR,
+    req.url === '/' ? 'index.html' : req.url,
+  );
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Not Found');
+    } else {
+      const contentType = getContentType(filePath);
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case '.html':
+      return 'text/html';
+    case '.css':
+      return 'text/css';
+    case '.js':
+      return 'text/javascript';
+    default:
+      return 'text/plain';
+  }
+}
